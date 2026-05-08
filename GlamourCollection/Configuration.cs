642bbe5a606a541b6f02251a9501f7ddb9ef48d0
ToxicStar8 <64957222+ToxicStar8@users.noreply.github.com>
2026-05-08 20:47:54 +0800

    Init

--- GlamourCollection/Configuration.cs
+++ GlamourCollection/Configuration.cs
@@ -11,50 +11,16 @@ namespace Main
     {
         public int Version { get; set; } = 0;
 
-        //检测范围
-        public int CheckRange { get; set; } = 2;
-        //检查间隔（毫秒）
-        public int CheckMillisecond { get; set; } = 200;
-        //是否显示提醒，在聊天界面
-        public bool IsShowTipsInChat { get; set; } = false;
-        //默语提醒
-        public string EchoTips { get; set; } = Lang.EchoTips;
-
-        //是否把好友也屏蔽了
-        public bool IsBlockFriend { get; set; } = false;
-        //是否把队友也屏蔽了
-        public bool IsBlockParty { get; set; } = false;
         //是否登录就显示窗口
         public bool IsLoginedOpenWindow { get; set; } = false;
         //是否使用Esc可以关闭窗口
         public bool IsEscCloseWindow { get; set; } = true;
-        //是否右键添加一个快捷方式
-        public bool IsRightClickAddShortcut { get; set; } = true;
-
-        //屏蔽的种族性别
-        public Dictionary<byte, RaceInfo> ByteToRace { get; set; }
-        //保存的预设
-        public Dictionary<byte, RaceInfo> TempByteToRace { get; set; }
-        //屏蔽的指定玩家 Key=CID Value=PlayerInfo
-        public Dictionary<ulong, PlayerInfo> BlockTargetRoleDic { get; set; }
-
         //语言类型 zh=0 todo:待实现
         public int LangIndex { get; set; } = 0;
 
         public void Init()
         {
-            BlockTargetRoleDic ??= new();
-            TempByteToRace ??= new();
-            ByteToRace ??= new() {
-            { 0, new RaceInfo(false,false) },
-            { 1, new RaceInfo(false,false) },
-            { 2, new RaceInfo(false,false) },
-            { 3, new RaceInfo(false,false) },
-            { 4, new RaceInfo(false,false) },
-            { 5, new RaceInfo(false,false) },
-            { 6, new RaceInfo(false,false) },
-            { 7, new RaceInfo(false,false) },
-            { 8, new RaceInfo(false,false) }};
+
         }
 
         public void Save()
--- GlamourCollection/Lang.cs
+++ GlamourCollection/Lang.cs
@@ -4,36 +4,12 @@ namespace Main
 {
     public static class Lang
     {
-        public static readonly string[] RaceName = ["未知", "人族", "精灵族", "拉拉菲尔族", "猫魅族", "鲁加族", "敖龙族", "硌狮族", "维埃拉族"];
         public static readonly string[] Sex = ["男", "女"];
-        public static readonly string AllSelect = "全选";
-        public static readonly string Reverse = "反选";
-        public static readonly string SavePresets = "保存预设";
-        public static readonly string ReadPresets = "读取预设";
-        public static readonly string SelectBlockRaceTitle = "选择你想屏蔽的种族";
-        public static readonly string Race = "种族";
-        public static readonly string TargetBlockList = "指定的屏蔽名单";
-        public static readonly string RoleName = "角色名";
-        public static readonly string WorldServer = "服务器";
-        public static readonly string Remove = "移除";
-        public static readonly string Setting = "设置";
-        public static readonly string CheckBlockRange = "附近屏蔽人数检测范围(m)";
-        public static readonly string CheckIntervalMillisecond = "屏蔽检测间隔(ms)";
-        public static readonly string EchoTipsTitle = "默语通知文本格式";
-        public static readonly string IsShowEchoTips = "是否启用默语提示";
-        public static readonly string IsBlockFriend = "是否屏蔽好友";
-        public static readonly string IsBlockParty = "是否屏蔽队友";
         public static readonly string LoginShow = "登录就显示窗口";
         public static readonly string EscClose = "Esc关闭窗口";
-        public static readonly string RightAddBlock = "右键添加快捷屏蔽";
         public static readonly string About = "关于";
         public static readonly string SendIssue = "反馈问题";
         public static readonly string OpenSetting = "打开设置窗口";
-        public static readonly string RemoveBlockList = "从屏蔽模型名单移除";
-        public static readonly string AddBlockList = "添加到屏蔽模型名单";
-        public static readonly string BlockNum = "已屏蔽: {0}";
-        public static readonly string TargetRole = "指定玩家";
-        public static readonly string BlackList = "黑名单";
-        public static readonly string EchoTips = "附近有屏蔽玩家靠近你了<se.1>";
+        public static readonly string Setting = "设置";
     }
 }
